0809f9d3c08cde6fa57474532514829a068f277e
Jesús A. Álvarez <zydeco@namedfork.net>
2022-12-11 13:35:03 +0100

    2022-12-11 part 2

--- 2022/d11/src/main.rs
+++ 2022/d11/src/main.rs
@@ -1,14 +1,19 @@
-use std::mem;
+use num_bigint::{BigUint, ToBigUint};
+use num_traits::Zero;
+use std::{
+    mem,
+    ops::{Mul, Rem, Shl},
+};
 
 struct Behaviour {
-    operation: fn(i32) -> i32,
-    divisor: i32,
+    operation: fn(&BigUint) -> BigUint,
+    divisor: u32,
     dst_true: usize,
     dst_false: usize,
 }
 
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<BigUint>,
     items_inspected: usize,
     behaviour: Behaviour,
 }
@@ -16,13 +21,17 @@ struct Monkey {
 impl Monkey {
     fn new(
         items: &[i32],
-        operation: fn(i32) -> i32,
-        divisor: i32,
+        operation: fn(&BigUint) -> BigUint,
+        divisor: u32,
         dst_true: usize,
         dst_false: usize,
     ) -> Monkey {
         Monkey {
-            items: items.into(),
+            items: items
+                .iter()
+                .map(i32::to_biguint)
+                .map(Option::unwrap)
+                .collect(),
             items_inspected: 0,
             behaviour: Behaviour {
                 operation,
@@ -36,33 +45,17 @@ impl Monkey {
 
 fn play_round(monkeys: &mut [Monkey]) {
     for i in 0..monkeys.len() {
-        println!("Monkey {}:", i);
         let items = mem::take(&mut monkeys[i].items);
         monkeys[i].items_inspected += items.len();
         let mb = &monkeys[i].behaviour;
-        for mut item in items {
-            println!("  Monkey inspects an item with a worry level of {}", item);
-            println!("    Worry level is transformed to {}", (mb.operation)(item));
-            item = (mb.operation)(item) / 3;
-            println!(
-                "    Monkey gets bored with item. Worry level is divided by 3 to {}.",
-                item
-            );
-            let dst = if item.rem_euclid(mb.divisor) == 0 {
-                println!("    Current worry level is divisible by {}.", mb.divisor);
+        for item in items {
+            let new_item = (mb.operation)(&item);
+            let dst = if new_item.clone().rem(mb.divisor).is_zero() {
                 mb.dst_true
             } else {
-                println!(
-                    "    Current worry level is not divisible by {}.",
-                    mb.divisor
-                );
                 mb.dst_false
             };
-            println!(
-                "    Item with worry level {} is thrown to monkey {}.",
-                item, dst
-            );
-            monkeys[dst].items.push(item);
+            monkeys[dst].items.push(new_item);
         }
     }
 }
@@ -70,11 +63,14 @@ fn play_round(monkeys: &mut [Monkey]) {
 fn play_rounds(rounds: usize, monkeys: &mut [Monkey]) {
     for r in 0..rounds {
         play_round(monkeys);
-        println!(
-            "After round {}, the monkeys are holding items with these worry levels:",
-            r + 1
-        );
-        print_state(monkeys);
+        if r == 0 || r == 19 || (r > 20 && r % 1000 == 999) {
+            println!("After round {}", r + 1);
+            print_inspected(monkeys);
+        }
+    }
+    if rounds % 1000 != 0 && rounds != 1 && rounds != 20 {
+        println!("After round {}", rounds);
+        print_inspected(monkeys);
     }
 }
 
@@ -103,30 +99,35 @@ fn business_level(monkeys: &[Monkey]) -> usize {
     inspected[0] * inspected[1]
 }
 
+macro_rules! pow2mod {
+    ($b:expr) => {{
+        |x| x.modpow(&BigUint::from(2u32), &BigUint::from($b))
+    }};
+}
+
 fn main() {
     // test
-    let mut monkeys0 = [
-        Monkey::new(&[79, 98], |old| old * 19, 23, 2, 3),
-        Monkey::new(&[54, 65, 75, 74], |old| old + 6, 19, 2, 0),
-        Monkey::new(&[79, 60, 97], |old| old * old, 13, 1, 3),
-        Monkey::new(&[74], |old| old + 3, 17, 0, 1),
+    let mut _monkeys0 = [
+        Monkey::new(&[79, 98], |old| old * 19u8, 23, 2, 3),
+        Monkey::new(&[54, 65, 75, 74], |old| old + 6u8, 19, 2, 0),
+        Monkey::new(&[79, 60, 97], pow2mod!(96577u32), 13, 1, 3),
+        Monkey::new(&[74], |old| old + 3u8, 17, 0, 1),
     ];
 
     // input
-    let mut monkeys1 = [
-        Monkey::new(&[72, 97], |old| old * 13, 19, 5, 6),
-        Monkey::new(&[55, 70, 90, 74, 95], |old| old * old, 7, 5, 0),
-        Monkey::new(&[74, 97, 66, 57], |old| old + 6, 17, 1, 0),
-        Monkey::new(&[86, 54, 53], |old| old + 2, 13, 1, 2),
-        Monkey::new(&[50, 65, 78, 50, 62, 99], |old| old + 3, 11, 3, 7),
-        Monkey::new(&[90], |old| old + 4, 2, 4, 6),
-        Monkey::new(&[88, 92, 63, 94, 96, 82, 53, 53], |old| old + 8, 5, 4, 7),
-        Monkey::new(&[70, 60, 71, 69, 77, 70, 98], |old| old * 7, 3, 2, 3),
+    let mut _monkeys1 = [
+        Monkey::new(&[72, 97], |old| old * 13u8, 19, 5, 6),
+        Monkey::new(&[55, 70, 90, 74, 95], pow2mod!(9699690u32), 7, 5, 0),
+        Monkey::new(&[74, 97, 66, 57], |old| old + 6u8, 17, 1, 0),
+        Monkey::new(&[86, 54, 53], |old| old + 2u8, 13, 1, 2),
+        Monkey::new(&[50, 65, 78, 50, 62, 99], |old| old + 3u8, 11, 3, 7),
+        Monkey::new(&[90], |old| old + 4u8, 2, 4, 6),
+        Monkey::new(&[88, 92, 63, 94, 96, 82, 53, 53], |old| old + 8u8, 5, 4, 7),
+        Monkey::new(&[70, 60, 71, 69, 77, 70, 98], |old| old * 7u8, 3, 2, 3),
     ];
 
-    let monkeys = &mut monkeys1;
+    let monkeys = &mut _monkeys1;
 
-    play_rounds(20, monkeys);
-    print_inspected(monkeys);
-    println!("Part1 busines level: {}", business_level(monkeys));
+    play_rounds(10000, monkeys);
+    println!("Busines level: {}", business_level(monkeys));
 }
