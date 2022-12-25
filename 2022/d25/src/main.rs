d0dd0ce4b7a7ba4b0d38cabfe7f23b3d2d6daf72
Jesús A. Álvarez <zydeco@namedfork.net>
2022-12-25 22:54:40 +0100

    2022-12-25 with arbitrary bases

--- 2022/d25/src/main.rs
+++ 2022/d25/src/main.rs
@@ -1,73 +1,77 @@
 use std::{
-    env,
     io::{self, BufRead},
+    ops::RangeInclusive,
+    vec,
 };
 
-fn decode_snafu(s: &str) -> i64 {
+const NEGATIVE_BASE_DIGITS: [char; 4] = ['-', '=', '!', '#'];
+
+fn decode_snafu(s: &str, base_range: &RangeInclusive<i8>) -> i64 {
+    assert!(base_range.contains(&0), "Base must contain 0");
+    let base = 1 + (base_range.end() - base_range.start()) as i64;
+    let positive_radix = (1 + *base_range.end()) as u32;
     s.chars()
         .rev()
         .enumerate()
         .map(|(idx, c)| {
-            let place = 5i64.pow(idx as u32);
+            let place = base.pow(idx as u32);
             place
                 * match c {
-                    '=' => -2,
-                    '-' => -1,
-                    '0' => 0,
-                    '1' => 1,
-                    '2' => 2,
+                    x if NEGATIVE_BASE_DIGITS.contains(&x) => {
+                        -(NEGATIVE_BASE_DIGITS.iter().position(|&p| p == x).unwrap() as i64 + 1)
+                    }
+                    x if x.is_digit(positive_radix) => x.to_digit(positive_radix).unwrap() as i64,
                     _ => panic!("Invalid digit {}", c),
                 }
         })
         .sum()
 }
 
-fn encode_snafu(i: i64) -> String {
+fn encode_snafu(i: i64, base_range: &RangeInclusive<i8>) -> String {
+    assert!(base_range.contains(&0), "Base must contain 0");
+    let base = 1 + (base_range.end() - base_range.start());
+    let max = *base_range.end();
     let mut r = i;
-    let mut place = 5i64.pow((i as f64).log(5f64).ceil() as u32);
-    let mut digits: Vec<char> = vec![];
-    while place > 0 {
-        let mut this_digit = r / place;
-        let next_place = place / 5;
-        println!(
-            "r={}, place={}[d{}], digit={}",
-            r,
-            place,
-            (place as f64).log(5f64).floor() as u32,
-            this_digit
-        );
-        if place > 1 {
-            let next_r = r - (this_digit * place);
-            println!(" next_r / next_place = {}", next_r / next_place);
-            if next_r / next_place > 2 {
-                println!("  this_digit +1");
-                this_digit += 1;
-            } else if next_r / next_place < -2 {
-                println!("  this_digit -1");
-                this_digit -= 1;
-            }
+    let mut digits: Vec<i8> = vec![];
+    // calculate digts
+    let mut place_value = 1i64;
+    for _ in 0i64.. {
+        let next_place_value = place_value * (base as i64);
+        let digit_value = r % next_place_value;
+        let digit = digit_value / place_value;
+        digits.push(digit as i8);
+        r -= digit_value;
+        if r == 0 {
+            break;
         }
-
-        r -= this_digit * place;
-        place = next_place;
-
-        assert!(
-            this_digit >= -2 && this_digit <= 2,
-            "digit was {}",
-            this_digit
-        );
-
-        digits.push(match this_digit {
-            -2 => '=',
-            -1 => '-',
-            0 => '0',
-            1 => '1',
-            2 => '2',
-            _ => unreachable!(),
-        });
+        place_value = next_place_value;
     }
+    // adjust for carry
+    digits.push(0);
+    for i in 0..digits.len() - 1 {
+        if digits[i] > max {
+            digits[i + 1] += 1;
+            digits[i] -= base;
+        }
+    }
+    // TODO: if number is negative, adjustment for undercarry might be needed
 
-    digits.iter().skip_while(|&d| d.eq(&'0')).collect()
+    let positive_radix = 1 + *base_range.end();
+    digits
+        .iter()
+        .rev()
+        .skip_while(|&&d| d == 0)
+        .map(|&d| match d {
+            x if x >= 0 && x < positive_radix => {
+                char::from_digit(d as u32, positive_radix as u32).unwrap()
+            }
+            x if x < 0 && x >= -(NEGATIVE_BASE_DIGITS.len() as i8) => {
+                NEGATIVE_BASE_DIGITS[(-x - 1) as usize]
+            }
+            x => panic!("Don't know how to represent {}", x),
+        })
+        .map(|c| String::from(c))
+        .collect()
 }
 
 fn read_input() -> Vec<String> {
@@ -76,12 +80,11 @@ fn read_input() -> Vec<String> {
 
 fn main() {
     let input = read_input();
+    let base_range = -2..=2;
 
-    let sum: i64 = input.iter().map(|ln| decode_snafu(&ln)).sum();
+    let sum: i64 = input.iter().map(|ln| decode_snafu(&ln, &base_range)).sum();
     println!("Sum: {}", sum);
-
-    let arg = env::args().skip(1).next().unwrap().parse::<i64>().unwrap();
-    let encoded = encode_snafu(arg);
-    println!("Encode {}: {}", arg, encoded);
-    assert_eq!(arg, decode_snafu(&encoded));
+    let encoded = encode_snafu(sum, &base_range);
+    println!("Encode: {}", encoded);
+    assert_eq!(sum, decode_snafu(&encoded, &base_range));
 }
