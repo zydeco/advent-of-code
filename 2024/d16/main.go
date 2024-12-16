6513681b19bd98ecedfa195687eea8d2b162d8a2
Jesús A. Álvarez <zydeco@namedfork.net>
2024-12-16 22:23:52 +0100

    2024-12-16 part 2, a bit buggy

--- 2024/d16/main.go
+++ 2024/d16/main.go
@@ -8,14 +8,6 @@ import (
 	"slices"
 )
 
-var verbose bool = os.Getenv("VERBOSE") == "1"
-
-func vprintf(format string, a ...any) {
-	if verbose {
-		fmt.Printf(format, a...)
-	}
-}
-
 type coord struct {
 	x, y int
 }
@@ -29,7 +21,7 @@ const (
 
 type board struct {
 	width, height int
-	walls         map[coord]bool
+	tiles         [][]tile
 	start, finish coord
 }
 
@@ -50,14 +42,9 @@ const (
 	TurnCounterClockwise
 )
 
-type reindeer struct {
-	pos    coord
-	facing direction
-}
-
 func readInput() board {
 	scanner := bufio.NewScanner(os.Stdin)
-	board := board{walls: make(map[coord]bool)}
+	board := board{tiles: make([][]tile, 0, 10)}
 
 	// read board
 	y := 0
@@ -70,35 +57,24 @@ func readInput() board {
 			board.width = len(line)
 		}
 
+		boardLine := make([]tile, len(line))
 		for x, z := range line {
 			xy := coord{x: x, y: y}
+			boardLine[x] = Empty
 			switch z {
 			case '#':
-				board.walls[xy] = true
+				boardLine[x] = Wall
 			case 'S':
 				board.start = xy
 			case 'E':
 				board.finish = xy
 			}
 		}
+		board.tiles = append(board.tiles, boardLine)
 		board.height += 1
 		y += 1
 	}
 	return board
-}
-
-func (d direction) back() direction {
-	switch d {
-	case North:
-		return South
-	case South:
-		return North
-	case West:
-		return East
-	case East:
-		return West
-	}
-	panic("invalid direction")
 }
 
 func (d direction) turnClockwise() direction {
@@ -160,7 +136,7 @@ func (c coord) next(dir direction) coord {
 
 func (b *board) isFree(c coord) bool {
 	// everything outside the board is walls
-	return b.isInBoard(c) && !b.walls[c]
+	return b.isInBoard(c) && b.tiles[c.y][c.x] == Empty
 }
 
 func (b *board) isInBoard(c coord) bool {
@@ -192,6 +168,33 @@ func (b board) String() string {
 	return s
 }
 
+func (b board) stringWithPath(path map[coord]bool) string {
+	s := " "
+	for x := 0; x < b.width; x++ {
+		s += fmt.Sprintf("%d", x%10)
+	}
+	s += "\n"
+	for y := 0; y < b.height; y++ {
+		s += fmt.Sprintf("%d", y%10)
+		for x := 0; x < b.width; x++ {
+			xy := coord{x: x, y: y}
+			if path[xy] {
+				s += "O"
+			} else if xy == b.start {
+				s += "S"
+			} else if xy == b.finish {
+				s += "E"
+			} else if !b.isFree(xy) {
+				s += "#"
+			} else {
+				s += "."
+			}
+		}
+		s += "\n"
+	}
+	return s
+}
+
 type visit struct {
 	cost   int
 	facing direction
@@ -208,7 +211,7 @@ func maybeVisit(b *board, visited *map[coord]visit, candidates *[]coord, cheapes
 	}
 }
 
-func part1(b board) {
+func part1(b board) int {
 	visited := make(map[coord]visit)
 	visited[b.start] = visit{cost: 0, facing: East}
 	cheapest := math.MaxInt
@@ -232,10 +235,126 @@ func part1(b board) {
 
 	result := visited[b.finish].cost
 	fmt.Println("Part 1:", result)
+	return result
+}
+
+func maybeVisit2(b *board, visited []int, candidates *[]position, cheapest *int, nextCoord coord, cost int, facing direction) {
+	next := position{
+		x:      nextCoord.x,
+		y:      nextCoord.y,
+		facing: facing,
+	}
+	currentCost := visited[b.posOrd(next)]
+	hasCost := currentCost < math.MaxInt
+	if b.isFree(nextCoord) && cost <= *cheapest && (!hasCost || cost <= currentCost) {
+		visited[b.posOrd(next)] = cost
+		*candidates = append(*candidates, next)
+		if nextCoord == b.finish && cost < *cheapest {
+			if *cheapest == math.MaxInt {
+				fmt.Printf("Found first cheapest: %d\n", cost)
+			}
+			*cheapest = cost
+		}
+	}
+}
+
+type position struct {
+	x, y   int
+	facing direction
+}
+
+func (c coord) facing(facing direction) position {
+	return position{x: c.x, y: c.y, facing: facing}
+}
+
+func (p position) coord() coord {
+	return coord{x: p.x, y: p.y}
+}
+
+func (b board) posOrd(p position) int {
+	return int(p.facing) + (4 * p.x) + (4 * b.width * p.y)
+}
+
+func (b board) ordPos(o int) position {
+	xy := o / 4
+	return position{
+		x:      xy % b.width,
+		y:      xy / b.width,
+		facing: direction(o % 4),
+	}
+}
+
+func repeated[E any](value E, times int) []E {
+	slice := make([]E, times)
+	for i := 0; i < times; i++ {
+		slice[i] = value
+	}
+	return slice
+}
+
+func (b board) costs(cheapest int) (int, map[coord]int) {
+	visited := repeated(math.MaxInt, b.width*b.height*4)
+	candidates := make([]position, 0, b.width*b.height*4)
+	candidates = append(candidates, b.start.facing(East))
+	visited[b.posOrd(candidates[0])] = 0
+	for len(candidates) > 0 {
+		var pos position
+		pos, candidates = candidates[0], candidates[1:]
+		cost := visited[b.posOrd(pos)]
+		maybeVisit2(&b, visited, &candidates, &cheapest, pos.coord().next(pos.facing), cost+1, pos.facing)
+		maybeVisit2(&b, visited, &candidates, &cheapest, pos.coord(), cost+1000, pos.facing.turnCounterClockwise())
+		maybeVisit2(&b, visited, &candidates, &cheapest, pos.coord(), cost+1000, pos.facing.turnClockwise())
+
+		// sort candidates
+		slices.SortStableFunc(candidates, func(p, q position) int {
+			return visited[b.posOrd(q)] - visited[b.posOrd(p)]
+		})
+	}
+
+	costs := make(map[coord]int)
+	for idx, cost := range visited {
+		if cost == math.MaxInt {
+			continue
+		}
+		pos := b.ordPos(idx)
+		if costs[pos.coord()] == 0 {
+			costs[pos.coord()] = cost
+		} else {
+			costs[pos.coord()] = min(costs[pos.coord()], cost)
+		}
+	}
+	return cheapest, costs
+}
+
+func (b board) reverse() board {
+	b2 := b
+	b2.start = b.finish
+	b2.finish = b.start
+	return b2
+}
+
+func part2(b board, cost int) {
+	cheapest1, costs1 := b.costs(cost)
+	cheapest2, costs2 := b.reverse().costs(cost)
+	if cheapest1 != cheapest2 {
+		panic("wtf")
+	}
+	path := make(map[coord]bool)
+	count := 0
+	for c, cost := range costs1 {
+		sum := cost + costs2[c]
+		if sum == cheapest1 || sum == cheapest1+1000 {
+			count += 1
+			path[c] = true
+		}
+	}
+	fmt.Println("Part 2", count) // off by 2? see picture
+	fmt.Println(b.stringWithPath(path))
 }
 
 func main() {
 	board := readInput()
 	fmt.Printf("Input:\n%s\n", board)
-	part1(board)
+	cost := part1(board)
+	part2(board, cost)
 }
